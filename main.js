5083bf9f0b8e2bb8bf7e1e19976441e478afd406
Byun0904 <yja05092@gmail.com>
2022-03-09 15:12:36 +0900

    node.js start

---
+++ main.js
@@ -0,0 +1,19 @@
+let http = require("http");
+let fs = require("fs");
+let url = require("url");
+
+let app = http.createServer(function (request, response) {
+  let _url = request.url;
+  let queryData = url.parse(_url, true).query;
+  if (_url == "/") {
+    _url = "/index.html";
+  }
+  if (_url == "/favicon.ico") {
+    response.writeHead(404);
+    response.end();
+    return;
+  }
+  response.writeHead(200);
+  response.end(queryData.id);
+});
+app.listen(3000);
