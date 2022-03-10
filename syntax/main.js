9840bba95d5fb43ce10a64374f6473e2cd25715e
Byun0904 <yja05092@gmail.com>
2022-03-10 21:52:59 +0900

    homepage

--- syntax/main.js
+++ syntax/main.js
@@ -5,38 +5,66 @@ let url = require("url");
 let app = http.createServer(function (request, response) {
   let _url = request.url;
   let queryData = url.parse(_url, true).query;
-  let title = queryData.id;
+  let pathName = url.parse(_url, true).pathname;
 
-  if (_url == "/") {
-    title = "Welcome";
-  }
-  if (_url == "/favicon.ico") {
+  if (pathName === "/") {
+    // 홈페이지인가를 판별
+    if (queryData.id === undefined) {
+      let title = "Welcome";
+      let description = "Hello, Node.js";
+      let template = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8" />
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+      </html>
+      `;
+      // 200은 성공적으로 접속됨
+      response.writeHead(200);
+      response.end(template);
+    } else {
+      fs.readFile(`../data/${queryData.id}`, "utf8", (err, description) => {
+        let title = queryData.id;
+        let template = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8" />
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+      </html>
+      `;
+        // 200은 성공적으로 접속됨
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
+  } else {
+    // 404는 연결이 되지 않음
     response.writeHead(404);
-    response.end();
-    return;
+    response.end("Not found");
   }
-  response.writeHead(200);
-  fs.readFile(`../data/${queryData.id}`, "utf8", (err, description) => {
-    let template = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8" />
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-  </html>
-  `;
-    response.end(template);
-  });
 });
 app.listen(3000);
