fc23e9e4c96cfe7246b10d7bf0839849eaa12ff2
Byun0904 <yja05092@gmail.com>
2022-03-11 23:25:47 +0900

    fileList, function

--- syntax/main.js
+++ syntax/main.js
@@ -2,6 +2,32 @@ let http = require("http");
 let fs = require("fs");
 let url = require("url");
 
+const templateHTML = (title, list, body) => {
+  return `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+`;
+};
+
+const templateList = (fileList) => {
+  let list = "<ul>";
+  for (let i = 0; i < fileList.length; i++) {
+    list += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+  }
+  list += "</ul>";
+  return list;
+};
+
 let app = http.createServer(function (request, response) {
   let _url = request.url;
   let queryData = url.parse(_url, true).query;
@@ -10,55 +36,33 @@ let app = http.createServer(function (request, response) {
   if (pathName === "/") {
     // 홈페이지인가를 판별
     if (queryData.id === undefined) {
-      let title = "Welcome";
-      let description = "Hello, Node.js";
-      let template = `
-        <!DOCTYPE html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8" />
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-      </html>
-      `;
-      // 200은 성공적으로 접속됨
-      response.writeHead(200);
-      response.end(template);
-    } else {
-      fs.readFile(`../data/${queryData.id}`, "utf8", (err, description) => {
-        let title = queryData.id;
-        let template = `
-        <!DOCTYPE html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8" />
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-      </html>
-      `;
+      fs.readdir("../data", (err, fileList) => {
+        let title = "Welcome";
+        let description = "Hello, Node.js";
+        let list = templateList(fileList);
+        let template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`
+        );
         // 200은 성공적으로 접속됨
         response.writeHead(200);
         response.end(template);
+      });
+    } else {
+      fs.readdir("../data", (err, fileList) => {
+        fs.readFile(`../data/${queryData.id}`, "utf8", (err, description) => {
+          let title = queryData.id;
+          let list = templateList(fileList);
+          let template = templateHTML(
+            title,
+            list,
+            `<h2>${title}</h2>${description}`
+          );
+          // 200은 성공적으로 접속됨
+          response.writeHead(200);
+          response.end(template);
+        });
       });
     }
   } else {
