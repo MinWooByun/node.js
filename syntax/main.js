fde1f81deba72f72a0930101e31e543a9f0f7682
Byun0904 <yja05092@gmail.com>
2022-03-13 23:26:16 +0900

    글생성 UI
    POST 방식으로 전송된 데이터 받기
    파일생성과 리다이렉션

--- syntax/main.js
+++ syntax/main.js
@@ -1,6 +1,7 @@
 let http = require("http");
 let fs = require("fs");
 let url = require("url");
+// let qs = require("querystring");
 
 const templateHTML = (title, list, body) => {
   return `
@@ -13,6 +14,7 @@ const templateHTML = (title, list, body) => {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
@@ -65,6 +67,42 @@ let app = http.createServer(function (request, response) {
         });
       });
     }
+  } else if (pathName === "/create") {
+    fs.readdir("../data", (err, fileList) => {
+      let title = "WEB - create";
+      let list = templateList(fileList);
+      let template = templateHTML(
+        title,
+        list,
+        `<form action="http://localhost:3000/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"/></p>
+          <p>
+            <textarea name="description" placeholder="discription"></textarea>
+          </p>
+          <p><input type="submit"/></p>
+         </form>`
+      );
+      // 200은 성공적으로 접속됨
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if (pathName === "/create_process") {
+    let body = "";
+
+    request.on("data", (data) => {
+      body += data;
+    });
+
+    request.on("end", () => {
+      // let post = qs.parse(body);
+      let title = new URLSearchParams(body).get("title");
+      let description = new URLSearchParams(body).get("description");
+      fs.writeFile(`../data/${title}`, description, "utf8", (err) => {
+        // 302는 리다이렉션이다.
+        response.writeHead(302, { Location: `/?id=${title}` });
+        response.end();
+      });
+    });
   } else {
     // 404는 연결이 되지 않음
     response.writeHead(404);
