69ceff43b2272328f5216b3436604065f7a960b0
Byun0904 <yja05092@gmail.com>
2022-03-15 20:58:13 +0900

    입출력 정보에 대한 보안

--- syntax/main.js
+++ syntax/main.js
@@ -1,6 +1,8 @@
 let http = require("http");
 let fs = require("fs");
 let url = require("url");
+let path = require("path");
+let sanitizeHtml = require("sanitize-html");
 // let qs = require("querystring");
 
 let template = require("../lib/template.js");
@@ -30,17 +32,24 @@ let app = http.createServer(function (request, response) {
       });
     } else {
       fs.readdir("../data", (err, fileList) => {
-        fs.readFile(`../data/${queryData.id}`, "utf8", (err, description) => {
+        // path를 쓰는 이유는 url를 조작하여 ..을 붙여 계속해서 상위폴더에 접근해 정보를 빼낼 수도 있기 때문이다.
+        let filteredId = path.parse(queryData.id).base;
+        fs.readFile(`../data/${filteredId}`, "utf8", (err, description) => {
           let title = queryData.id;
+          // sanitize-html을 쓰는 이유는 작성자가 악의적으로 script 태그등을 기입하여 마음대로 조작할 수도 있기 때문이다.
+          let sanitizedTitle = sanitizeHtml(title);
+          let sanitizeDescription = sanitizeHtml(description, {
+            allowedTags: ["h1"],
+          });
           let list = template.list(fileList);
           let html = template.html(
-            title,
+            sanitizedTitle,
             list,
-            `<h2>${title}</h2>${description}`,
+            `<h2>${sanitizedTitle}</h2>${sanitizeDescription}`,
             `<a href="/create">create</a>
-             <a href="/update?id=${title}">update</a>
+             <a href="/update?id=${sanitizedTitle}">update</a>
              <form action="/delete_process" method="post">
-              <input type="hidden" name="id" value="${title}"/>
+              <input type="hidden" name="id" value="${sanitizedTitle}"/>
               <input type="submit" value="delete"/>
              </form>`
           );
@@ -90,7 +99,8 @@ let app = http.createServer(function (request, response) {
     });
   } else if (pathName === "/update") {
     fs.readdir("../data", (err, fileList) => {
-      fs.readFile(`../data/${queryData.id}`, "utf8", (err, description) => {
+      let filteredId = path.parse(queryData.id).base;
+      fs.readFile(`../data/${filteredId}`, "utf8", (err, description) => {
         let title = queryData.id;
         let list = template.list(fileList);
         let html = template.html(
@@ -141,7 +151,8 @@ let app = http.createServer(function (request, response) {
     request.on("end", () => {
       // let post = qs.parse(body);
       let id = new URLSearchParams(body).get("id");
-      fs.unlink(`../data/${id}`, (err) => {
+      let filteredId = path.parse(id).base;
+      fs.unlink(`../data/${filteredId}`, (err) => {
         response.writeHead(302, { Location: `/` });
         response.end();
       });
